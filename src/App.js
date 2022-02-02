70f74b873b5dc771bd4edcebbcbc639b028594a0
dallynowens2 <dallynowens2@gmail.com>
2022-02-02 11:48:41 -0700

    lamport clock

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import Peer from 'peerjs';
+import { useEffect, useState } from "react";
+import Peer from "peerjs";
 
 const randId = () => {
   let roomLength = 5;
@@ -11,109 +11,131 @@ const randId = () => {
     return String.fromCharCode(r);
   };
   return [...new Array(roomLength).keys()].map(randChar).join("");
-}
+};
 
 function App() {
-  const [peerId, setPeerId] = useState('');
-  const [message, setMessage] = useState('');
-  const [recepients, setRecepients] = useState('');
-
-  const [peer, setPeer] = useState(new Peer(randId()))
+  const [peerId, setPeerId] = useState("");
+  const [message, setMessage] = useState("");
+  const [recepients, setRecepients] = useState("");
+  const [lamportClock, setLamportClock] = useState(1);
+  const [peer, setPeer] = useState(new Peer(randId()));
   const [connections, setConnections] = useState([]);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    peer.on('open', (id) => {
-      console.log('My id: ' + id)
-    })
-    peer.on('connection', (conn) => {
+    peer.on("open", (id) => {
+      console.log("My id: " + id);
+    });
+    peer.on("connection", (conn) => {
       configureConnection(conn);
-      console.log('Connected to peer: ' + conn.peer);
-    })
-  }, [peer])
+      console.log("Connected to peer: " + conn.peer);
+    });
+  }, [peer]);
 
   const addPeer = () => {
     const conn = peer.connect(peerId);
     console.log("Connecting to: ", conn.peer);
 
     configureConnection(conn);
-    setPeerId('');
-  }
+    setPeerId("");
+  };
 
   const configureConnection = (conn) => {
-    conn.on('open', () => {
-      conn.on('data', (data) => {
+    conn.on("open", () => {
+      conn.on("data", (data) => {
         console.log(data);
-        setMessages(prev => [...prev, data])
-      })
+        if (data.tempLamportClock > lamportClock) {
+          setLamportClock(data.tempLamportClock + 1);
+        } else {
+          setLamportClock(lamportClock + 1);
+        }
+        setMessages((prev) => [...prev, data]);
+      });
 
-      setConnections(prev => [...prev, conn]);
+      setConnections((prev) => [...prev, conn]);
     });
-  }
+  };
 
   const sendMessageHandler = () => {
     const messageObj = {
       message,
       timeSent: new Date(),
       author: peer.id,
-      sentBy: peer.id
-    }
+      sentBy: peer.id,
+      tempLamportClock: lamportClock + 1,
+    };
     sendMessage(messageObj);
-    setMessages(prev => [...prev, messageObj]);
-    setMessage('')
-  }
+    setMessages((prev) => [...prev, messageObj]);
+    setMessage("");
+  };
 
   const sendMessage = (m) => {
     console.log(m);
+    setLamportClock(lamportClock + 1);
     if (recepients.trim().length > 0) {
       const rec = recepients.split(",");
-      rec.forEach(r => connections[r - 1].send(m));
+      rec.forEach((r) => connections[r - 1].send(m));
+    } else {
+      connections.forEach((c) => c.send(m));
     }
-    else {
-      connections.forEach(c => c.send(m));
-    }
-  }
+  };
 
   const forwardMessage = (m) => {
-    const newM = {...m}
-    newM.sentBy = peer.id
+    const newM = { ...m };
+    newM.sentBy = peer.id;
     sendMessage(newM);
-  }
+  };
 
   const connectPeer = (id) => {
     const conn = peer.connect(id);
     console.log("Connecting to: ", conn.peer);
     configureConnection(conn);
-  }
+  };
 
   const messageChangeHandler = (e) => {
-    setMessage(e.target.value)
-  }
+    setMessage(e.target.value);
+  };
   const onPeerIdChange = (e) => {
-    setPeerId(e.target.value)
-  }
+    setPeerId(e.target.value);
+  };
   const onRecepientChange = (e) => {
     setRecepients(e.target.value);
-  }
+  };
+  const onEventClickhandler = () => {
+    setLamportClock(lamportClock + 1);
+  };
 
   return (
     <div className="App">
       <h2>My Id:</h2>
       <p>{peer.id}</p>
       <div>
-        <label>Peer Id</label><br />
-        <input type='text' value={peerId} onChange={onPeerIdChange} />
-        <button type='button' onClick={addPeer}>Add Peer</button>
+        <label>Peer Id</label>
+        <br />
+        <input type="text" value={peerId} onChange={onPeerIdChange} />
+        <button type="button" onClick={addPeer}>
+          Add Peer
+        </button>
       </div>
       <div>
-        <label>Enter Recepients. NOTE: please separate them with commas (e.g. 1, 2, 3)</label><br />
-        <input type='text' value={recepients} onChange={onRecepientChange} />
-        <p>Message will be sent to: {recepients.trim().length > 0 ? recepients : "All"}</p>
+        <label>
+          Enter Recepients. NOTE: please separate them with commas (e.g. 1, 2,
+          3)
+        </label>
+        <br />
+        <input type="text" value={recepients} onChange={onRecepientChange} />
+        <p>
+          Message will be sent to:{" "}
+          {recepients.trim().length > 0 ? recepients : "All"}
+        </p>
       </div>
       <div>
-        <label>Enter Message</label><br />
-        <input type='text' value={message} onChange={messageChangeHandler} />
-        <button type='button' onClick={sendMessageHandler}>Send Message</button>
+        <label>Enter Message</label>
+        <br />
+        <input type="text" value={message} onChange={messageChangeHandler} />
+        <button type="button" onClick={sendMessageHandler}>
+          Send Message
+        </button>
       </div>
       <div>
         <h4>Connections</h4>
@@ -122,24 +144,35 @@ function App() {
             <div key={i}>
               <p>{`${i + 1}) ${c.peer}`}</p>
             </div>
-          )
+          );
         })}
+      </div>
+      <div>
+        <button type="button" onClick={onEventClickhandler}>
+          Event
+        </button>
+        <br />
+        Lamport Clock: {lamportClock}
       </div>
       <div>
         <h4>Messages</h4>
         {messages.map((m, i) => {
           return (
             <div key={`m_${i}`}>
-              <span>{`${i + 1}) ${m.sentBy}: ${m.message}`}</span>
-              <button type='button' onClick={() => forwardMessage(m)}>Forward</button>
+              <span>{`Lamport Clock <${m.tempLamportClock}, ${m.sentBy}>: ${m.message}`}</span>
+              <button type="button" onClick={() => forwardMessage(m)}>
+                Forward
+              </button>
               {m.sentBy != m.author && (
                 <>
                   <span>Connect to {m.author}?</span>
-                  <button type='button' onClick={() => connectPeer(m.author)}>Connect</button>
+                  <button type="button" onClick={() => connectPeer(m.author)}>
+                    Connect
+                  </button>
                 </>
               )}
             </div>
-          )
+          );
         })}
       </div>
     </div>
